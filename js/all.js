307853a6c2cf73f6281a8313284db0d052c93abc
Milla <artoriaaa@gmail.com>
2019-06-21 19:38:08 +0800

    some functions and hover styles

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ $(document).ready(function($) {
 	$('.hambur').click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
-		$('body').toggleClass('active');
+		$('body').toggleClass('toggle');
 
 	});
 
@@ -18,9 +18,39 @@ $(document).ready(function($) {
 	});
 
 	//sidebar
-	$('.category a').click(function(event) {
+
+	$('.category li').click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
+		if($(this).hasClass('c1')){
+			
+			$('.c_all_products').css('display','block');
+			$('.c_all_products').siblings('.itemList').css('display','none');
+
+		}
+		if($(this).hasClass('c2')){
+			
+			$('.c_franch_fries').css('display','block');
+			$('.c_franch_fries').siblings('.itemList').css('display','none');
+
+		}
+		if($(this).hasClass('c3')){
+			
+			$('.c_instant').css('display','block');
+			$('.c_instant').siblings('.itemList').css('display','none');
+
+		}
+		if($(this).hasClass('c4')){
+			
+			$('.c_hamburgers').css('display','block');
+			$('.c_hamburgers').siblings('.itemList').css('display','none');
+
+		}
+
+		
+
 		
 	});
 
@@ -28,19 +58,52 @@ $(document).ready(function($) {
 	$('.loveBox').click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
-		/* var $heart = $('.heartBox>img');
-		    if ($heart.attr('src','assets/heart_w.svg')) {
-		        $heart.attr('src','assets/heart_line.svg');
-		    } 
-		    else {
-		       // $listSort.attr('colspan', 6);
-		    }*/
-		$(this).find('img').attr('src','assets/heart_w.svg')
+		//$(this).find('img').attr('src','assets/heart_w.svg');
+		$(this).toggleClass('heart_w');
+	});
 
+	$('.addToCart').click(function(event) {
+		event.preventDefault();
+	});
+
+
+	//購物車數量
+	$('.caculate').click(function(){
+		event.preventDefault();
+	//var i=$('div').size();//获得div标签的数目
+	//var x=$('.heart_w').size();   //获得class为mm的font标签的数目
+	//$('#quantity').html('購物車'+x+'件物品');
+	//alert('好');
 
 	});
 
 
+
+
+
+});
+
+$(function(){   
+
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        var catecrtop=$(".cart_content .sidebar").offset().top;
+        if(scrollVal>adscrtop){
+        	alert('好');
+        }
+
+      /*  if(window.innerWidth>767){ //RWD 767以下寬不動作
+	         if(scrollVal>adscrtop){
+	            $(".div-img").css({"position": "fixed","top": "0px"});          
+	         }else{
+	            $(".div-img").css({"position": "static"});
+	         }
+
+	    }else{
+         	$(".div-img").css({"position": "static"});
+        }*/
+
+    });
 
 
 });
